dae1ea19a961ad25b82b80f7e27044a166fad68a
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-16 18:43:43 +0900

    add: bad testing: 11,12,13

--- synctime-plot.sh
+++ synctime-plot.sh
@@ -12,6 +12,9 @@ FILE_NAME_7="debug7.log.csv"
 FILE_NAME_8="debug8.log.csv"
 FILE_NAME_9="debug9.log.csv"
 FILE_NAME_10="debug10.log.csv"
+FILE_NAME_11="debug11-9b5c0cb.log.csv"
+FILE_NAME_12="debug12-827b0bd.log.csv"
+FILE_NAME_13="debug13-cee8630.log.csv"
 
 # FILE_NAME_1="uncached_IBD.log.csv"
 # FILE_NAME_2="cached_IBD.log.csv"
@@ -61,7 +64,10 @@ plot \
 "$FILE_NAME_7" using 2:3 axis x1y1 w l title "$FILE_NAME_7(cee8630)" lc rgb "#00ffcc" lw 1.0, \
 "$FILE_NAME_8" using 2:3 axis x1y1 w l title "$FILE_NAME_8(7dd2e3e)" lc rgb "#ccff00" lw 1.0, \
 "$FILE_NAME_9" using 2:3 axis x1y1 w l title "$FILE_NAME_9(8683428)" lc rgb "#cc00cc" lw 1.0, \
-"$FILE_NAME_10" using 2:3 axis x1y1 w l title "$FILE_NAME_10(f9d0998)" lc rgb "#00aabb" lw 1.0,
+"$FILE_NAME_10" using 2:3 axis x1y1 w l title "$FILE_NAME_10(f9d0998)" lc rgb "#00aabb" lw 1.0, \
+"$FILE_NAME_11" using 2:3 axis x1y1 w l title "$FILE_NAME_11" lc rgb "#aa00bb" lw 1.0, \
+"$FILE_NAME_12" using 2:3 axis x1y1 w l title "$FILE_NAME_12" lc rgb "#aacc00" lw 1.0, \
+"$FILE_NAME_13" using 2:3 axis x1y1 w l title "$FILE_NAME_13" lc rgb "#f0ccf0" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
