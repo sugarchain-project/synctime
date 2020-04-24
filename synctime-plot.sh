32aa1a24e3156ac1e011766c3150d7e75728dd8e
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-04-25 01:29:13 +0900

    add: okfast(barrystyle) 2x vs 8x

--- synctime-plot.sh
+++ synctime-plot.sh
@@ -38,6 +38,10 @@ FILE_NAME_22="debug22-barry-4x_2nd.log.csv"
 FILE_NAME_23="debug23-barry-8x.log.csv"
 FILE_NAME_24="debug24-barry-2x.log.csv"
 
+# okfast
+FILE_NAME_25="debug25-okfast2x.log.csv"
+FILE_NAME_26="debug26-okfast8x.log.csv"
+
 ## DRAW PLOT
 OUTPUT_PNG="./png/synctime.png"
 gnuplot -persist <<-EOFMarker
@@ -69,11 +73,8 @@ set key box opaque;
 set key noenhanced;
 
 plot \
-"$FILE_NAME_20" using 2:3 axis x1y1 w l title "$FILE_NAME_20" lc rgb "#af0000" lw 1.0, \
-"$FILE_NAME_21" using 2:3 axis x1y1 w l title "$FILE_NAME_21" lc rgb "#af00af" lw 1.0, \
-"$FILE_NAME_22" using 2:3 axis x1y1 w l title "$FILE_NAME_22" lc rgb "#afaf00" lw 1.0, \
-"$FILE_NAME_23" using 2:3 axis x1y1 w l title "$FILE_NAME_23" lc rgb "#00afaf" lw 1.0, \
-"$FILE_NAME_24" using 2:3 axis x1y1 w l title "$FILE_NAME_24" lc rgb "#00af00" lw 1.0,
+"$FILE_NAME_25" using 2:3 axis x1y1 w l title "$FILE_NAME_25" lc rgb "#a0a0fc" lw 1.0, \
+"$FILE_NAME_26" using 2:3 axis x1y1 w l title "$FILE_NAME_26" lc rgb "#afa0fc" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
