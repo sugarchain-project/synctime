c9466d0f0bf86c1b7a0bcf159c8064c04548ba94
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-05-11 17:02:11 +0900

    skip, more getheaders

--- synctime-plot.sh
+++ synctime-plot.sh
@@ -41,6 +41,52 @@ FILE_NAME_24="debug24-barry-2x.log.csv"
 # okfast
 FILE_NAME_25="debug25-okfast2x.log.csv"
 FILE_NAME_26="debug26-okfast8x.log.csv"
+FILE_NAME_27="debug27-okfast2x_2nd.log.csv"
+FILE_NAME_28="debug28-okfast2x_3rd.log.csv"
+FILE_NAME_29="debug29-okfast2x_4th.log.csv"
+
+# only skip
+FILE_NAME_30="debug30-skip.log.csv"
+FILE_NAME_31="debug31-skip_2nd.log.csv"
+FILE_NAME_32="debug32-skip_3rd.log.csv"
+FILE_NAME_33="debug33-move.log.csv"
+FILE_NAME_34="debug34-split.log.csv"
+FILE_NAME_35="debug35-moonlight.log.csv"
+FILE_NAME_36="debug36-skip_4th.log.csv"
+
+# move
+FILE_NAME_37="debug37-temp.log.csv"
+FILE_NAME_38="debug38-move.log.csv"
+FILE_NAME_39="debug39-move_2nd.log.csv"
+
+# temp
+FILE_NAME_40="debug40-moreHeaders.log.csv"
+FILE_NAME_41="debug41-nolog.log.csv"
+FILE_NAME_42="debug42-reindexC.log.csv"
+
+# skip
+FILE_NAME_43="debug43-skip.log.csv"
+FILE_NAME_44="debug44-skip-RC.log.csv"
+FILE_NAME_45="debug45-skip2.log.csv"
+FILE_NAME_46="debug46-moonlight.log.csv"
+FILE_NAME_47="debug47-skip-R.log.csv"
+
+# last
+FILE_NAME_50="debug50-moonlight.log.csv"
+FILE_NAME_51="debug51-skip.log.csv"
+FILE_NAME_52="debug52-reindex.log.csv"
+FILE_NAME_53="debug53-reindex-chainstate.log.csv"
+FILE_NAME_54="debug54-skip2.log.csv"
+
+# revert#80
+FILE_NAME_60="debug60-1d5a8c.log.csv"
+FILE_NAME_61="debug61-revert80.log.csv"
+
+# revert78
+FILE_NAME_63="debug63-mb.log.csv"
+FILE_NAME_64="debug64-MHR_2.log.csv"
+FILE_NAME_65="debug65-MHR_4.log.csv"
+FILE_NAME_66="debug66_MHR_2_2nd.log.csv"
 
 ## DRAW PLOT
 OUTPUT_PNG="./png/synctime.png"
@@ -73,8 +119,10 @@ set key box opaque;
 set key noenhanced;
 
 plot \
-"$FILE_NAME_25" using 2:3 axis x1y1 w l title "$FILE_NAME_25" lc rgb "#a0a0fc" lw 1.0, \
-"$FILE_NAME_26" using 2:3 axis x1y1 w l title "$FILE_NAME_26" lc rgb "#afa0fc" lw 1.0,
+"$FILE_NAME_50" using 2:3 axis x1y1 w l title "$FILE_NAME_50" lc rgb "#000000" lw 1.0, \
+"$FILE_NAME_63" using 2:3 axis x1y1 w l title "$FILE_NAME_63" lc rgb "#cf0000" lw 1.0, \
+"$FILE_NAME_64" using 2:3 axis x1y1 w l title "$FILE_NAME_64" lc rgb "#00cf00" lw 1.0, \
+"$FILE_NAME_66" using 2:3 axis x1y1 w l title "$FILE_NAME_66" lc rgb "#00cfcf" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
