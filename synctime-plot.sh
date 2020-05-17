c219b37af1d723a2726e39667b578291eb1af124
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-05-18 01:22:01 +0900

    v32 win64

--- synctime-plot.sh
+++ synctime-plot.sh
@@ -96,6 +96,7 @@ FILE_NAME_71="debug71-b2000.log.csv"
 FILE_NAME_72="debug72-move2000.log.csv"
 FILE_NAME_73="debug73-v31.log.csv"
 FILE_NAME_74="debug74-v32.log.csv"
+FILE_NAME_75="debug75-v32-w64.log.csv"
 
 ## DRAW PLOT
 OUTPUT_PNG="./png/synctime.png"
@@ -129,12 +130,10 @@ set key noenhanced;
 
 plot \
 "$FILE_NAME_50" using 2:3 axis x1y1 w l title "$FILE_NAME_50" lc rgb "#000000" lw 1.0, \
-"$FILE_NAME_63" using 2:3 axis x1y1 w l title "$FILE_NAME_63" lc rgb "#000000" lw 1.0, \
-"$FILE_NAME_64" using 2:3 axis x1y1 w l title "$FILE_NAME_64" lc rgb "#00cf00" lw 1.0, \
-"$FILE_NAME_66" using 2:3 axis x1y1 w l title "$FILE_NAME_66" lc rgb "#00cfcf" lw 1.0, \
 "$FILE_NAME_71" using 2:3 axis x1y1 w l title "$FILE_NAME_71" lc rgb "#000000" lw 1.0, \
 "$FILE_NAME_73" using 2:3 axis x1y1 w l title "$FILE_NAME_73" lc rgb "#ccffcc" lw 1.0, \
 "$FILE_NAME_74" using 2:3 axis x1y1 w l title "$FILE_NAME_74" lc rgb "#ff0000" lw 1.0, \
+"$FILE_NAME_75" using 2:3 axis x1y1 w l title "$FILE_NAME_75" lc rgb "#aaddaa" lw 1.0, \
 # caution at the end: no "\"
 EOFMarker
 
